bafdc96389d41041521f3450be27e6928b49c71c
Taras <example@gmail.com>
2021-02-04 13:16:46 +0200

    first commit

---
+++ JS/script.js
@@ -0,0 +1,21 @@
+ "use strict"
+
+//alert('Hello');
+
+//const result = confirm('Aer you here?');
+//console.log(result);
+
+//const answer = prompt('Вам є 18?', '');
+//console.log(typeof(answer));
+
+//const answers = []
+
+//answers[0] = prompt('What is your name?', '');
+//answers[1] = prompt('What is surename?', '');
+//answers[2] = prompt('What is your age?', '');
+
+//document.write(answers);
+
+const user = 'Taras';
+alert(`Hello, ${user}`);
+    
