a1e3c7a093e2458499d4fe3639e4b35973257442
Taras <example@gmail.com>
2021-02-04 15:48:52 +0200

    home work1

--- JS/script.js
+++ JS/script.js
@@ -16,6 +16,35 @@
 
 //document.write(answers);
 
-const user = 'Taras';
-alert(`Hello, ${user}`);
+//const user = 'Taras';
+//alert(`Hello, ${user}`);
     
+// Home work
+
+const numberOfFilms = +prompt('How many movies have you seen?', '');
+
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {}, 
+    actors: {}, 
+    genres: [], 
+    privat: false
+};
+
+const a = prompt('One of the last watched movies?', ''),
+      b = prompt('How would tou rate it?', ''),
+      c = prompt('One of the last watched movies?', ''),
+      d = prompt('How would tou rate it?', '');
+
+      personalMovieDB.movies[a] = b;
+      personalMovieDB.movies[c] = d;
+
+      console.log(personalMovieDB);
+
+
+
+
+
+      
+
